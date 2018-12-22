5a6a9d87c3ad874c39dc8a2e9e9bac261008d872
Gap2003 <44299820+Gap2003@users.noreply.github.com>
2018-12-22 16:58:04 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+alert ("Hello Billy")
+prompt ("How may I judge your GD skill today?")
+alert ("click anywhere to judge")
+function change (){document.getElementById("skill").innerHTML = "Potato"};
+document.addEventListener ('click', change);
